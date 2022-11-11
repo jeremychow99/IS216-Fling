1bc8e4577770ba248e7678ca96e147f76c1365ac
Leroy Tham <leroy.tham.2021@scis.smu.edu.sg>
2022-11-11 17:38:41 +0800

    navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,11 @@
   <div>
   <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">
+        <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top mx-3" alt="Kitten">
+        Fling
+      </a>
+
       <button
         class="navbar-toggler"
         type="button"
@@ -16,41 +20,37 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <li class="nav-item me-3">
             <router-link class="btn btn-default" :to="{ name: 'Home' }">
               <i class="fa-solid fa-house-user fs-3"></i>
               <h6 class="nav-title">Home</h6>
             </router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item me-3">
             <router-link class="btn btn-default" :to="{ name: 'Events' }">
               <i class="fa-solid fa-calendar-days fs-3"></i>
               <h6 class="nav-title">Events</h6>
             </router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item me-3">
             <router-link class="btn btn-default" :to="{ name: 'Chats' }">
               <i class="fa-solid fa-message fs-3"></i>
-              <h6 class="nav-title">Chats</h6>
+              <h6 class="nav-title ">Chats</h6>
             </router-link>
           </li>
+        </ul>
+        <ul class="navbar-nav ms-auto me-3 mb-3">
           <li class="nav-item">
-            <router-link class="btn btn-default" :to="{ name: 'signupFinalised' }">
-          <h6 class="nav-title">Signup Page</h6>
-        </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="btn btn-default" :to="{ name: 'Login' }">
-          <h6 class="nav-title">Login Page</h6>
-        </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="btn btn-default" :to="{ name: 'Setup' }">
-          <h6 class="nav-title">Profile Setup</h6>
-        </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="btn btn-default" :to="{ name: 'Login' }"><button @click="logoutFunc(); $emit('logout')">logout</button></router-link>
+            <div class="dropdown d-flex">
+              <button class="btn btn-secondary dropdown-toggle mt-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Picture UserName 
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Route to Profile</a></li>
+                <li><a class="dropdown-item" href="#">Route to edit bio etc</a></li>
+                <li><a class="dropdown-item" @click="logoutFunc(); $emit('logout')"><router-link :to="{ name: 'Login' }">Logout</router-link></a></li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
@@ -81,3 +81,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.nav-item:hover{
+  color:blue;
+}
+.router-link-active{
+  text-decoration:underline;
+}
+</style>
