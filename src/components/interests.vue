dec750afa80760993ab92bd825b647e65a8d58c0
Deborah How <hyh.deb@gmail.com>
2022-11-11 15:08:43 +0800

    added multiselect interests and year

--- src/components/interests.vue
+++ src/components/interests.vue
@@ -1,23 +1,43 @@
 <template>
-
-<p class="display-4 text-center">Interests</p>
-    <div class="col-lg-6"></div>
-    <div class="container border border-dark col-lg-6 col-sm-6">
-        <p>Choose up to 5:</p>
-        <div class="d-flex flex-row m-2">
-            <input type="checkbox" class="btn-check" id="item1" autocomplete="off" style="overflow-wrap: break-word;">
-            <label class="btn btn-outline-dark p-2 me-2" style="overflow-wrap: break-word;" for="item1">Cybersecurity</label><br>
-            <input type="checkbox" class="btn-check" id="item2" autocomplete="off">
-            <label class="btn btn-outline-dark p-2 me-2" for="item2">Artificial Intelligence</label><br>
-            <input type="checkbox" class="btn-check" id="item3" autocomplete="off">
-            <label class="btn btn-outline-dark p-2 me-2" for="item3">FinTech</label><br>            
-            <input type="checkbox" class="btn-check" id="item4" autocomplete="off">   
-            <label class="btn btn-outline-dark p-2 me-2 col-2" for="item4">Data Science</label><br>
-            <input type="checkbox" class="btn-check" id="item5" autocomplete="off">
-            <label class="btn btn-outline-dark p-2 col-lg-2" for="item5">Investment Banking</label><br>
-        </div>   
-        </div>
-
-    <div class="col-lg-6 col-sm-6"></div>
-
+    <div>
+        <p>Your Interests</p>
+        <Multiselect mode="multiple" placeholder="Choose your interests" :options="options" />
+    </div>
 </template>
+  
+<script>
+import Multiselect from '@vueform/multiselect'
+
+export default {
+    components: { Multiselect },
+    data() {
+        return {
+            userInterests: [],
+            options: [
+                "Artificial Intelligence",
+                "Blockchain",
+                "Business Intelligence",
+                "Cyber Security",
+                "Data Science",
+                "Econometrics",
+                "Entrepreneurship",
+                "Finance",
+                "FinTech",
+                "Investment Banking",
+                "Law",
+                "Machine Learning",
+                "Management",
+                "Marketing",
+                "Mathematics",
+                "Political Science",
+                "Psychology",
+                "Software Engineering",
+                "Web 3.0"],
+        }
+    }
+}
+</script>
+  
+<style src="@vueform/multiselect/themes/default.css">
+
+</style>
