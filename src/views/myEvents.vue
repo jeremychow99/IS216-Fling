94cf91863869281eb2d0426b40328879609cdf25
jeremy chow <xelloxzxd@gmail.com>
2022-11-13 02:07:54 +0800

    implement  add event and delete event fixes

--- src/views/myEvents.vue
+++ src/views/myEvents.vue
@@ -4,8 +4,8 @@
   <div v-if="!isLoading" class="container justify-content-center">
     <ul class="nav nav-tabs justify-content-center">
       <li class="nav-item">
-          <router-link class="btn btn-default" :to="{ name: 'Events' }">
-            Events Feed
+        <router-link class="btn btn-default" :to="{ name: 'Events' }">
+          Events Feed
         </router-link>
       </li>
       <li class="nav-item">
@@ -20,9 +20,9 @@
           class="btn btn-primary my-2"
           data-bs-toggle="modal"
           data-bs-target="#exampleModal"
-          style="float: right;"
+          style="float: right"
         >
-        <i class="fa-solid fa-plus"></i> Add Another Event
+          <i class="fa-solid fa-plus"></i> Add Another Event
         </button>
       </div>
     </div>
@@ -44,14 +44,121 @@
         </myEventCard>
       </div>
     </div>
-    <div v-if="this.events.length == 0"><h1 class="display-2 text-center">You have not created any events!</h1></div>
+    <div v-if="this.events.length == 0">
+      <h1 class="display-2 text-center">You have not created any events!</h1>
+    </div>
+  </div>
 
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add Event</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Event Name</span>
+            <input
+              v-model="inputName"
+              type="text"
+              class="form-control"
+              aria-label="eventname"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1"
+              >Event Location</span
+            >
+            <input
+              v-model="inputLocation"
+              type="text"
+              class="form-control"
+              aria-label="eventname"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <label for="basic-url" class="form-label"></label>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon3">Description</span>
+            <textarea
+              v-model="inputDetails"
+              type="text"
+              class="form-control"
+              id="basic-url"
+              aria-describedby="basic-addon3"
+              placeholder="100 Character Limit"
+            />
+          </div>
+          <div class="input-group mb-3" id="inputGroupPrepend">
+            <span class="input-group-text">Date</span>
+            <input
+              v-model="inputDate"
+              type="date"
+              class="form-control"
+              aria-label="date"
+            />
+          </div>
+
+          <div class="input-group mb-3">
+            <span class="input-group-text">Time</span>
+            <input
+              v-model="inputTime"
+              type="time"
+              class="form-control"
+              aria-label="time"
+            />
+          </div>
+
+          <div class="input-group">
+            <span class="input-group-text">Details</span>
+            <textarea
+              v-model="inputDesc"
+              class="form-control"
+              aria-label="With textarea"
+              placeholder="500 Character Limit"
+              maxlength="500"
+            ></textarea>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            data-bs-dismiss="modal"
+            class="btn btn-primary"
+            @click="addEvent"
+          >
+            Add Event
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { db } from "../config";
-import { getDocs, collection, deleteDoc } from "firebase/firestore";
+import { getDocs, collection, addDoc, deleteDoc } from "firebase/firestore";
 import myEventCard from "../components/myEventCard.vue";
 import Navbar from "../components/Navbar.vue";
 import LoadingScreen from "../components/loading.vue";
@@ -77,22 +184,54 @@ export default {
     };
   },
   methods: {
-    deleteEvent: function (s){
-      console.log(s)
-      for (let index = 0; index < this.events.length; index++) {
-        if(this.events[index].eventID== s){
-          this.events.splice(index,1)
+    addEvent: async function () {
+      // console.log(this.inputTime)
+      // console.log(this.inputDate)
+      // console.log(typeof this.inputTime)
+      // console.log(typeof this.inputDate)
+      const docRef = await addDoc(collection(db, "events"), {
+        eventName: this.inputName,
+        eventDate: this.inputDate,
+        eventTime: this.inputTime,
+        eventDesc: this.inputDesc,
+        eventDetails: this.inputDetails,
+        eventLocation: this.inputLocation,
+        creator: this.$store.state.user.displayName,
+        creatorEmail: this.$store.state.user.email,
+      });
+      const addedObj = {
+        eventID: docRef.id,
+        eventDate: this.inputDate,
+        eventDetails: this.inputDetails,
+        eventName: this.inputName,
+        eventLocation: this.inputLocation,
+        eventTime: this.inputTime,
+        eventDesc: this.inputDesc,
+        eventCreator: this.$store.state.user.displayName,
+      };
+      console.log(docRef.id)
+      this.events.push(addedObj);
+    },
+    deleteEvent: function (s) {
+      console.log(s);
+      setTimeout(() => {
+        for (let index = 0; index < this.events.length; index++) {
+          if (this.events[index].eventID == s) {
+            this.events.splice(index, 1);
+          }
         }
-        
-      }
-      // for(let item of this.events){
-      //   console.log(item)
-      //   if(item.eventID == s){
-      //     this.events.pop(item)
+      }, 1200);
+      //   for (let index = 0; index < this.events.length; index++) {
+      //   if (this.events[index].eventID == s) {
+      //     this.events.splice(index, 1);
       //   }
       // }
+      // for (let index = 0; index < this.events.length; index++) {
+      //   if (this.events[index].eventID == s) {
+      //     this.events.splice(index, 1);
+      //   }
     },
-   
+
     logoutUser: function () {
       setTimeout(() => {
         this.isLoading = true;
@@ -126,7 +265,7 @@ export default {
       if (eventObj.eventCreator == this.$store.state.user.displayName) {
         this.events.push(eventObj);
       }
-      console.log(this.events)
+      console.log(this.events);
     });
   },
 };
